669f673ddc67eb1c059e03cf0fb6ed2ee67796ec
Crowdbotics <team@crowdbotics.com>
2021-09-13 07:53:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lehs_30496Identifier",
+  url: "https://lehs_30496Identifier.botics.co",
+  api: "https://lehs_30496Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
